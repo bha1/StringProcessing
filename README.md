cce187b8e933f076254a9ad350c366979177148a
bha1 <peeyushbhawan@gmail.com>
2015-12-29 23:05:24 +0530

    badPractise inspite of creating newRepo trying to reuse older one

--- README.md
+++
@@ -1,2 +0,0 @@
-# StringProcessing
-some useful string algorithms exposed over restful web services
---
+++ src/string/html/chief/HelloWorld.java
@@ -0,0 +1,9 @@
+package string.html.chief;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("First class java");
+	}
+
+}
