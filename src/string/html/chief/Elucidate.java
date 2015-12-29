d7ad083c4fe2a47b1964f179974975c37260f381
bha1 <peeyushbhawan@gmail.com>
2015-12-29 23:13:57 +0530

    redesigning of elucidate

---
+++ src/string/html/chief/Elucidate.java
@@ -0,0 +1,11 @@
+package string.html.chief;
+
+public class Elucidate {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to elucidate 2.0 !!");
+		System.out.println("We will help you understand things better");
+
+	}
+
+}
